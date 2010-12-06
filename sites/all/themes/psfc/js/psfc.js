2a742d3e87e96fe23b0f92cb2216f3ffc5697731
aaron <aaron>
2010-12-06 18:50:28 +0000

    Admin can register attendee manually. Don't use behaviors on the cufon button js, it breaks views ui.

--- sites/all/themes/psfc/js/psfc.js
+++ sites/all/themes/psfc/js/psfc.js
@@ -2,10 +2,10 @@
 * Convert input buttons to span for cufon. Hide the input, append the div, span
 * and button value, bind the click.
 */
-Drupal.behaviors.psfcCufon = function(context) {
+$(document).ready(function() {
   $('input.button').each(function(){
       $(this).hide().after('<div class="button-rounded">').next('div.button-rounded').html('<span>' + $(this).val() + '</span>').click(function(){
           $(this).prev('input.button').click();
       });
   });
-}
+});
