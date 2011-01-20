48f167c8b2cab12207bde36d2ac9575b16c19041
notjonathan <notjonathan>
2011-01-20 20:10:24 +0000

    replaced disclosure triangles with lightweight version

--- sites/all/themes/psfc/js/psfc.js
+++ sites/all/themes/psfc/js/psfc.js
@@ -1,19 +1,60 @@
+$(function() {
+
 /**
 * Convert input buttons to span for cufon. Hide the input, append the div, span
 * and button value, bind the click.
 */
-$(document).ready(function() {
-  $('input.button').each(function(){
-      $(this).hide().after('<div class="button-rounded">').next('div.button-rounded').html('<span>' + $(this).val() + '</span>').click(function(){
-          $(this).prev('input.button').click();
-      });
-  });
-});
 
-/**
- * Collapse the "menus" used as help text for js enabled browsers
- */
-$(document).ready(function() {
-    $('div#main-content div.content ul.menu li.collapsed').addClass('start-collapsed');
+	$('input.button').each(function(){
+		$(this).hide().after('<div class="button-rounded">').next('div.button-rounded').html('<span>' + $(this).val() + '</span>').click(function(){
+			 $(this).prev('input.button').click();
+		});
+	});
+
+
+/////////////////////////////
+//
+// slightly simpler disclosure triangles
+//
+//	  outer container any block element with class QA
+//   question container is <a> with class of "Q"
+//   answer container is any block element with class of "A"
+//
+// dependent upon gfx: /misc/menu-expanded.png & menu-collapsed.png
+//
+
+	$(".QA").each(function(){
+		var qa = $(this);
+		var q = $(this).children(".Q");
+		var a = $(this).children(".A");
+
+		qa.wrapInner("<ul class='qa_ul'/>");
+		a.wrap("<li class='a_li' style='padding:0px; margin-left:10px;'/>");
+
+		var ap = a.parent();
+		ap.isOpen = true;
+		
+		q.wrap("<li class='q_li' style='list-style-type: none, list-style-position: outside; padding:0px; margin-left:10px'/>").attr("href","javascript:void 0");
+		q.click(function(){toggle()});
+
+		var qp = q.parent();
+		
+		function toggle(quick){
+			if (ap.isOpen){
+				ap.hide(quick?0:"medium");
+				qp.css("list-style-image","url(/coop/misc/menu-collapsed.png)");
+			} else {
+				ap.show("medium");
+				qp.css("list-style-image","url(/coop/misc/menu-expanded.png)");
+			}
+			ap.isOpen = !ap.isOpen;
+		}
+		
+		toggle(true);
+		
+	});
+	
+//
+/////////////////////////////
 
 });
