96489d75e9882393e51dc61f4265299748f463c2
nat <nat>
2011-02-02 18:57:16 +0000

    Multiple changes to the administrative interface.

--- sites/all/themes/psfc/template.php
+++ sites/all/themes/psfc/template.php
@@ -58,3 +58,67 @@ function psfc_button($element) {
   return '<input type="submit" ' . (empty($element['#name']) ? '' : 'name="' . $element['#name'] . '" ') . 'id="' . $element['#id'] . '" value="' . check_plain($element['#value']) . '" ' . drupal_attributes($element['#attributes']) . " />\n";
 
 }
+
+
+function psfc_views_bulk_operations_fields_action_form($form) {
+  if (arg(0) == 'admin' && arg(1) == 'psfc' && arg(2) == 'orientation' && arg(3) == 'attendee-manage') {
+    drupal_set_title(t('Change attendence status for attendees'));
+  }
+  $output = '';
+  $access = user_access('Use PHP input for field settings (dangerous - grant with care)');
+  if ($access && $form['#settings']['php_code']) {
+    $output .= t('<h3>Using the Code widget</h3>
+                  <ul>
+                  <li>Will override the value specified in the Field widget.</li>
+                  <li>Should include &lt;?php ?&gt; delimiters.</li>
+                  <li>If in doubt, refer to <a target="_blank" href="@link_devel">devel.module</a> \'Dev load\' tab on a content page to figure out the expected format.</
+li>
+                  </ul>', array('@link_devel' => 'http://www.drupal.org/project/devel')
+                 );
+  }
+
+  if (count($form['#field_info']) == 1) { // Special case for just one field: make the table more usable
+    $field_name = key($form['#field_info']);
+    $header = array();
+    if ($form[$field_name . '_add']['#type'] == 'checkbox') {
+      $row[] = drupal_render($form[$field_name . '_add']);
+      $header[] = array('data' => t('Add?'), 'title' => t('Checkboxes in this column allow you to add new values to multi-valued fields, instead of overwriting existing values.'));
+    }
+    $row[] = drupal_render($form[$field_name]);
+    $header[] = t('Field');
+    if ($access && $form['#settings']['php_code']) {
+      $row[] = drupal_render($form[$field_name . '_code']);
+      $header[] = t('Code');
+    }
+    if (count($header) == 1) {
+      $header = NULL;
+    }
+    $output .= theme('table', $header, array(array('class' => 'fields-action-row', 'id' => 'fields-action-row' . str_replace('_', '-', $field_name), 'data' => $row)));
+  }
+  else { // Many fields
+    drupal_add_js(drupal_get_path('module', 'views_bulk_operations') . '/fields.action.js');
+
+    $header = array(theme('table_select_header_cell'), t('Add?'), t('Field'));
+    if ($access && $form['#settings']['php_code']) {
+      $header[] = t('Code');
+    }
+    if (!empty($form['#field_info'])) foreach ($form['#field_info'] as $field_name => $field) {
+        $row = array(
+                     'class' => 'fields-action-row',
+                     'id' => 'fields-action-row-' . str_replace('_', '-', $field_name), 
+                     'data' => array(
+                                     drupal_render($form[$field_name . '_check']),
+                                     drupal_render($form[$field_name . '_add']),
+                                     drupal_render($form[$field_name]),
+                                     ),
+                     );
+        if ($access && $form['#settings']['php_code']) {
+          $row['data'][] = drupal_render($form[$field_name . '_code']);
+        }
+        $rows[] = $row;
+      }
+    $output .= theme('table', $header, $rows); 
+  }
+  $output .= drupal_render($form);
+  return $output;
+}
