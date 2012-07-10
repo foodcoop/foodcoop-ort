c414cc0ada8b2fff3269480ef16efff6f2f0b5a8
natm <natm>
2012-07-10 18:30:46 +0000

    Slight chages to the theme code (including cleanup).

--- sites/all/themes/psfc/template.php
+++ sites/all/themes/psfc/template.php
@@ -6,6 +6,8 @@
 function psfc_preprocess_page(&$variables) {
   global $user;
   $variables['primary_links'] = psfc_output_primary($variables['primary_links']);
+  // TODO Please change this theme call to use an associative array for the $variables parameter.
+  // TODO Please change this theme call to use an associative array for the $variables parameter.
   $variables['orientation_crumbs'] = theme('psfc_orientation_crumbs', $_GET['q']);
 }
 
@@ -21,7 +23,11 @@ function psfc_preprocess_block(&$variables) {
   }
 }
 
- function psfc_output_primary($primary_links) {
+/**
+ * @todo Please document this function.
+ * @see http://drupal.org/node/1354
+ */
+function psfc_output_primary($primary_links) {
   $out = '<ul id="nav">';
   // create an array to map css classes, for background image in links
   $class_names = array(
@@ -29,14 +35,14 @@ function psfc_preprocess_block(&$variables) {
     'Overview' => 'join',
     'News & Publications' => 'news',
     'Food & Recipies' => 'food',
-   'Events & Community' =>  'events',
-   'Member Information' => 'members',
+    'Events & Community' => 'events',
+    'Member Information' => 'members',
   );
 
   foreach ($primary_links as $key => $menu_item) {
     $attributes = array('class' => $class_names[$menu_item['title']]);
     $link = l($menu_item['title'], $menu_item['href'], array('attributes' => $attributes));
-    $out .= '<li class="grid_2">'. $link .'</li>';
+    $out .= '<li class="grid_2">' . $link . '</li>';
   }
   $out .= '</ul>';
 
@@ -45,13 +51,17 @@ function psfc_preprocess_block(&$variables) {
 
 // Adding the class "button" so we can target to replace with cufon text in
 // psfc.js
+/**
+ * @todo Please document this function.
+ * @see http://drupal.org/node/1354
+ */
 function psfc_button($element) {
   // Make sure not to overwrite classes.
   if (isset($element['#attributes']['class'])) {
-    $element['#attributes']['class'] = 'form-' . $element['#button_type'] . ' ' . $element['#attributes']['class']  .' button';
+    $element['#attributes']['class'] = 'form-' . $element['#button_type'] . ' ' . $element['#attributes']['class']  . ' button';
   }
   else {
-    $element['#attributes']['class'] = 'form-' . $element['#button_type'] .' button';
+    $element['#attributes']['class'] = 'form-' . $element['#button_type'] . ' button';
   }
 
   return '<input type="submit" ' . (empty($element['#name']) ? '' : 'name="' . $element['#name'] . '" ') . 'id="' . $element['#id'] . '" value="' . check_plain($element['#value']) . '" ' . drupal_attributes($element['#attributes']) . " />\n";
@@ -59,6 +69,10 @@ function psfc_button($element) {
 }
 
 
+/**
+ * @todo Please document this function.
+ * @see http://drupal.org/node/1354
+ */
 function psfc_views_bulk_operations_fields_action_form($form) {
   if (arg(0) == 'admin' && arg(1) == 'psfc' && arg(2) == 'orientation' && arg(3) == 'attendee-manage') {
     drupal_set_title(t('Change attendence status for attendees'));
@@ -81,7 +95,10 @@ li>
     $header = array();
     if ($form[$field_name . '_add']['#type'] == 'checkbox') {
       $row[] = drupal_render($form[$field_name . '_add']);
-      $header[] = array('data' => t('Add?'), 'title' => t('Checkboxes in this column allow you to add new values to multi-valued fields, instead of overwriting existing values.'));
+      $header[] = array(
+        'data' => t('Add?'),
+        'title' => t('Checkboxes in this column allow you to add new values to multi-valued fields, instead of overwriting existing values.'),
+      );
     }
     $row[] = drupal_render($form[$field_name]);
     $header[] = t('Field');
@@ -92,31 +109,45 @@ li>
     if (count($header) == 1) {
       $header = NULL;
     }
-    $output .= theme('table', $header, array(array('class' => 'fields-action-row', 'id' => 'fields-action-row' . str_replace('_', '-', $field_name), 'data' => $row)));
+    $output .= theme('table', array('header' => array(
+        'header' => $header,
+        'rows' => array(array(
+            'class' => 'fields-action-row',
+            'id' => 'fields-action-row' . str_replace('_', '-', $field_name),
+            'data' => $row,
+          )),
+      )));
   }
   else { // Many fields
     drupal_add_js(drupal_get_path('module', 'views_bulk_operations') . '/fields.action.js');
 
+    // TODO Please change this theme call to use an associative array for the $variables parameter.
+    // TODO Please change this theme call to use an associative array for the $variables parameter.
     $header = array(theme('table_select_header_cell'), t('Add?'), t('Field'));
     if ($access && $form['#settings']['php_code']) {
       $header[] = t('Code');
     }
-    if (!empty($form['#field_info'])) foreach ($form['#field_info'] as $field_name => $field) {
+    if (!empty($form['#field_info'])) {
+      foreach ($form['#field_info'] as $field_name => $field) {
         $row = array(
-                     'class' => 'fields-action-row',
-                     'id' => 'fields-action-row-' . str_replace('_', '-', $field_name),
-                     'data' => array(
-                                     drupal_render($form[$field_name . '_check']),
-                                     drupal_render($form[$field_name . '_add']),
-                                     drupal_render($form[$field_name]),
-                                     ),
-                     );
+          'class' => 'fields-action-row',
+          'id' => 'fields-action-row-' . str_replace('_', '-', $field_name),
+          'data' => array(
+            drupal_render($form[$field_name . '_check']),
+            drupal_render($form[$field_name . '_add']),
+            drupal_render($form[$field_name]),
+          ),
+        );
         if ($access && $form['#settings']['php_code']) {
           $row['data'][] = drupal_render($form[$field_name . '_code']);
         }
         $rows[] = $row;
       }
-    $output .= theme('table', $header, $rows);
+    }
+    $output .= theme('table', array('header' => array(
+        'header' => $header,
+        'rows' => $rows,
+      )));
   }
   $output .= drupal_render($form);
   return $output;
